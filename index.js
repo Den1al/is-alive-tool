d0fef3d77845f08cd177aff97ba395d461d2dab4
Daniel Abeles <abeles22@gmail.com>
2019-04-26 18:32:15 +0300

    inital commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import { testAllURLS, allTrue } from './lib/utils';
+import { printEpilog, printProlog } from './lib/prints';
+
+async function main() {
+  const urls = [
+    'https://facebook.com',
+    'https://twitter.com',
+    'https://reddit.com',
+  ];
+
+  printProlog();
+
+  const allAlive = allTrue(await testAllURLS(urls));
+
+  printEpilog(allAlive);
+}
+
+main().catch(console.error);
